2d379256feb21e4cb3d19fb9096d24c7b5deb9ce
Maggieboa <achymag2020@gmail.com>
2023-06-18 08:36:50 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - start of our program
+ *
+ *
+ *
+ *
+ *Return: returns results after for loop statements
+ */
+int main(void)
+{
+
+	char alph;
+
+	while (alph != 'e' || alph != 'q')
+	{
+		putchar(alph);
+		alph++;
+	}
+		putchar('\n');
+	return (0);
+}
