c450b2d46edab0edc20744d5c2168250052a8192
Maggieboa <achymag2020@gmail.com>
2023-06-18 07:54:13 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - where our program starts to run from
+ *
+ *
+ *
+ *
+ *Return: returns results after the for loop statement
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph = < 'z'; alph++)
+	{
+	putchar(char);
+	}
+	for (alph = 'A'; alph = < 'Z'; alph++)
+	{
+	putchar(char);
+	}
+	putchar('\n');
+return (0);
+}
