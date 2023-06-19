d561a78276e63655f5ca99b93caccfc3a92ba93d
Maggieboa <achymag2020@gmail.com>
2023-06-19 02:20:29 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - starting point of our orogram
+ *
+ *
+ *Return: returns our value after the for loop statemrents
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
