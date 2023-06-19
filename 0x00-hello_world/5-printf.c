30895d84f845c5144042cc752840b21de4eadac6
Maggieboa <achymag2020@gmail.com>
2023-06-19 15:05:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ *main - starting point of our program
+ *
+ *
+ *
+ *
+ *Return: returns the results after the program terminates
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
