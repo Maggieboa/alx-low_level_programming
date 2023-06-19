e9d1b0f7c4c2d433f6885c62ef0b3a2715b286dd
Maggieboa <achymag2020@gmail.com>
2023-06-19 16:15:37 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,15 +11,14 @@ int main(void)
 {
 
 	char alph;
-	
+
 	for (alph = 'a'; alph <= 'z'; alph++)
 	{
-	  	
 	if (alph != 'e' && alph != 'q')
 	{
 		putchar(alph);
 	}
-	}	
+	}
 	putchar('\n');
 	return (0);
 }
