317e526aff751cd7f317788943c26dceb3d434dc
Maggieboa <achymag2020@gmail.com>
2023-06-19 15:31:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *main - starting point of our program
+ *
+ *
+ *
+ *
+ *Return: retuns the value  of our program
+ **/
+int main(void)
+{
+	char startp[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(startp);
+	return (0);
+}
