1a29d7154f88604ac4980e5abb73145130785824
Maggieboa <achymag2020@gmail.com>
2023-06-24 10:52:39 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ *mul - starting point of ou program
+ *@a: local variable
+ *@b:local variale
+ *
+ *Return: retuns either 1 or 0
+ **/
+int mul(int a, int b)
+{
+	int num;
+
+	num = a * b;
+	return (num);
+}
