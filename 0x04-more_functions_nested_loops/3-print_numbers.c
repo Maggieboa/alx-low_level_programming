6092b508ab7053d89f471ca1bd880fd4120630ab
Maggieboa <achymag2020@gmail.com>
2023-06-24 11:02:30 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ *print_numbers - begining of our program
+ *
+ *
+ *
+ *Return: returns either 0 for success or 1 if not successful
+ **/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
