8956fad190b2ee41a684f7ced94d27651347e167
Maggieboa <achymag2020@gmail.com>
2023-06-25 06:41:36 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet - our starting point for our program
+*
+*
+*Return: void
+**/
+void print_alphabet(void)
+{
+char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
