664f26d993a0464b5517d7adcd09d38945c5ff07
Maggieboa <achymag2020@gmail.com>
2023-06-25 04:34:47 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include"main.h"
+#include<stdio.h>
+/**
+*main - start of of our program
+*
+*
+*
+*
+*Return: either 0 or 1
+*
+**/
+int main(void)
+{
+	int x;
+
+for (x = 1; x <= 100; x++)
+
+{
+	if (x%3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (x%5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else
+	{
+		printf("%d ", x);
+	}
+}
+	printf("\n");
+return (0);
+}
