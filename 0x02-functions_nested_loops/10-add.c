44e36a4bb86b8ef37a0f3ba3099368f8e57d92c5
Maggieboa <achymag2020@gmail.com>
2023-06-25 09:35:01 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+*add -  starting point of our addition program
+*
+*@int: data type to be returned
+*
+*Return: stores the result of the addition
+**/
+int add(int x, int y )
+{
+	return (x + y);
+}
