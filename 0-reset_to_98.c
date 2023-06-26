dea2e2ab1bff415f4583bbfbca9d7398f519c055
Maggieboa <achymag2020@gmail.com>
2023-06-26 06:28:49 -0700

    0-reset_to_98.c

---
+++ 0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+*reset_to_98 - starting point of our program
+*
+*@n: variable
+*
+*Return: its empty so the return value is void
+**/
+void reset_to_98(int *n)
+{
+	int x = 98;
+	*n = x;
+
+}
