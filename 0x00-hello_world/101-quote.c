00c0575e3f3f85699a0ce6cd9f0b4992c55d7152
Maggieboa <achymag2020@gmail.com>
2023-06-28 03:04:23 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+/**
+*main -  starting point of our program
+*Return: should return 1
+**/
+/**
+*section1 - the first sectons1
+*Return: returns 0
+**/
+/**
+*section2 - the second section2
+*Return: 0 for success
+**/
+int main(void)
+{
+section1();
+section2();
+putchar('\n');
+
+return (1);
+}
+int section1(void)
+{
+	putchar('a');
+	putchar('n');
+	putchar('d');
+	putchar(' ');
+	putchar('t');
+	putchar('h');
+	putchar('a');
+	putchar('t');
+	putchar(' ');
+	putchar('p');
+	putchar('i');
+	putchar('e');
+	putchar('c');
+	putchar('e');
+	putchar(' ');
+	putchar('o');
+	putchar('f');
+	putchar(' ');
+	putchar('a');
+	putchar('r');
+	putchar('t');
+	putchar(' ');
+	putchar('i');
+	putchar('s');
+	putchar(' ');
+	putchar('u');
+	putchar('s');
+	putchar('e');
+	putchar('f');
+	putchar('u');
+
+	return (0);
+}
+int section2(void)
+{
+	putchar('l');
+	putchar('"');
+	putchar(' ');
+	putchar('D');
+	putchar('o');
+	putchar('r');
+	putchar('a');
+	putchar(' ');
+	putchar('K');
+	putchar('o');
+	putchar('r');
+	putchar('p');
+	putchar('a');
+	putchar('r');
+	putchar(',');
+	putchar(' ');
+	putchar('2');
+	putchar('0');
+	putchar('1');
+	putchar('5');
+	putchar('-');
+	putchar('1');
+	putchar('0');
+	putchar('-');
+	putchar('1');
+	putchar('9');
+
+	return (0);
+}
