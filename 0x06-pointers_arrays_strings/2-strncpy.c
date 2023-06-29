2689e6c4b6c5f2856343a0b93767688d5b99bb43
Maggieboa <achymag2020@gmail.com>
2023-06-29 15:08:27 -0700

    -2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,31 @@
 #include"main.h"
 /**
-*_strncpy - this is a function copies a string and works the same as the strncpy
-*
-*@dest: its a variable placeholder
+*_strncpy-this is a function copies a string
+*and works the same as the strncpy
+**@dest:its a variable placeholder
 *@src:variable placeholder of type char
 *@n:variable placeholder of type int
-*Return: returns dest value on success
+*Return:returns dest value on success
+*
 **/
 char *_strncpy(char *dest, char *src, int n)
 {
-	int length = 0, src_length = 0;
+int length;
+int src_length;
 
-	while (src[length++])
-	{
-		src_length++;
-	}
-	for (length = 0; src[length] && length < n; length++)
-	{
-		dest[length] = src[length];
-	}
-	for (length = src_length; length < n; length++)
-	{
-		dest[length] = '\0';
-	}
+length = 0;
+src_length = 0;
+while (src[length++])
+{
+	src_length++;
+}
+for (length = 0; src[length] && length < n; length++)
+{
+	dest[length] = src[length];
+}
+for (length = src_length; length < n; length++)
+{
+	dest[length] = '\0';
+}
 	return (dest);
 }
