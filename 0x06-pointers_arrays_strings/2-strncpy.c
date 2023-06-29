549d102eb9af3afbacf8b7277e6c3d28203e5b83
Maggieboa <achymag2020@gmail.com>
2023-06-29 10:33:58 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+*_strncpy - this is a function copies a string and works the same as the strncpy
+*
+*@dest: its a variable placeholder
+*@src:variable placeholder of type char
+*@n:variable placeholder of type int
+*Return: returns dest value on success
+**/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int length = 0, src_length = 0;
+
+	while (src[length++])
+	{
+		src_length++;
+	}
+	for (length = 0; src[length] && length < n; length++)
+	{
+		dest[length] = src[length];
+	}
+	for (length = src_length; length < n; length++)
+	{
+		dest[length] = '\0';
+	}
+	return (dest);
+}
