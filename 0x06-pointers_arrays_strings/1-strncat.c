0737a0aa07fc7f772a1a54f6d1cd398cf322da28
Maggieboa <achymag2020@gmail.com>
2023-06-29 15:57:53 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,30 @@
 #include"main.h"
 /**
-*_strcat - function predefined 
+*_strncat - function predefined
 *in the main.h header file
 *@dest: variable
 *@src: variable
+*@n:variable
 *Return: 0 for success and 1 if not successful
 **/
 char *_strncat(char *dest, char *src, int n)
 {
-	int length, dest_length;
-	
-	length = 0;
-	dest_length = 0;
-	while (dest[length++])
-	{
-		dest_length++;
-	}
-	if (dest[length] && length <= n)
-	{
-		dest[dest_length++] = src[length++];
-	}
-	else
-	{
-		dest[dest_length + n + 1] = src[length - 1];
-	}
-	return (dest);
+int length;
+int dest_length;
+
+length = 0;
+dest_length = 0;
+while (dest[length++])
+{
+	dest_length++;
+}
+if (dest[length] && length <= n)
+{
+	dest[dest_length++] = src[length++];
+}
+else
+{
+	dest[dest_length + n + 1] = src[length - n + 1];
+}
+return (dest);
 }
