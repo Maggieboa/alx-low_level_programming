85c89b526fa259ea972f7fe6340a13e6fb7e5c06
Maggieboa <achymag2020@gmail.com>
2023-06-30 09:18:51 -0700

    3-strcp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+*_strcmp - main function.
+*
+*@s1: placeholder variable
+*@s2:placeholder variable
+*
+*
+*Return: if successfull returns either the negative, positive or 0 in the first unmatched number
+**/
+int _strcmp(char *s1, char *s2)
+{
+int length;
+
+while (*s1 && *s2 && *s1 == *s2)
+{
+	s1++;
+	s2++;
+}
+return (*s1 - *s2);
+}
