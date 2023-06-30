ea9c2edc0a5e3680709a4148cec88414a0a5a21c
Maggieboa <achymag2020@gmail.com>
2023-06-30 15:46:11 -0700

    100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* rot13 - beginning of our program
+*@s: variable placeholder
+*
+*Return: the value held by variasble s
+**/
+char *rot13(char *s)
+{
+	int length = 0;
+
+while (length[a])
+{
+	while ((s[length] >= 'a' && s[length] <= 'z') || (s[length] >= 'A' && s[length] <= 'Z'))
+	{
+		if ((s[length] > 'm' && s[length] <= 'z') || (s[length] > 'M' && s[length] <= 'Z'))
+		{
+			s[length] -= 13;
+			break;
+		}
+	s[length] += 13;
+	break;
+		}
+a++;
+}
+return (s);
+}
+
