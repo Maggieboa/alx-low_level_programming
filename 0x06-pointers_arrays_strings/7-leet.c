bbd075145c42d59c6eaae09dc19e195cd6c08484
Maggieboa <achymag2020@gmail.com>
2023-06-30 08:38:08 -0700

    7-leetc.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include"main.h"
+/**
+*leet-function that convert the characters into leet numerals
+*
+*@s: variable placeholder for our array
+*
+*
+*Return:the actual value of s
+*
+**/
+char *leet(char *s)
+{
+	int length;
+	int length2;
+	char x[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	
+	length = 0;
+while (s[length])
+{
+
+	for (length2 = 0; length2 <= 7; length2++)
+	{
+		if (s[length] == x[length2] || s[length] - 32 == x[length2])
+		{
+			s[length] = length2 + '0';
+		}
+	}
+length++;
+}
+return (s);
+}
