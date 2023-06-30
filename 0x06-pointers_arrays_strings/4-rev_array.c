3fc5ac1987fcc0997957f1d6cb7f2d42bab241ef
Maggieboa <achymag2020@gmail.com>
2023-06-30 03:11:59 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+*reverse_array - function to write an array in reverse
+*
+*@a: variable
+*@n: variable for the placeholder of length
+*
+*
+*Return: void therefore its empty
+**/
+void reverse_array(int *a, int n)
+{
+int arr_length;
+int x;
+int y;
+
+y = 0;
+x = 0;
+for (arr_length = 0; a[arr_length] <= n; arr_length++)
+{
+	y = n - 1;
+}
+for (arr_length = 0; arr_length <= n / 2; arr_length++)
+{
+	x = a[arr_length];
+	a[arr_length] = a[y];
+	a[y] = x;
+	y--;
+}
+}
