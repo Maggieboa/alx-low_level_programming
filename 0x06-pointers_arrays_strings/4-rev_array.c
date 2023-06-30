9035d9802b0e5d26491e26bc4d01de3df4eeb18b
Maggieboa <achymag2020@gmail.com>
2023-06-30 03:49:50 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,11 +14,13 @@ int arr_length;
 int x;
 int y;
 
+y = 0;
+x = 0;
 for (arr_length = 0; a[arr_length] <= n; arr_length++)
 {
 	y = n - 1;
 }
-for (arr_length = 0; arr_length <= n / 2; arr_length++)
+for (arr_length = 0; arr_length < n / 2; arr_length++)
 {
 	x = a[arr_length];
 	a[arr_length] = a[y];
