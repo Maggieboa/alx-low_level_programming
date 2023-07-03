9bd6fe6fa933d1ecc92b82bed068997a8800d106
Maggieboa <achymag2020@gmail.com>
2023-07-03 10:39:24 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/*
+ *_strchr-main function
+ *
+ *@s:placeholder variable
+ *@c:placeholder variable for the char
+ *
+ *
+ *Return: 0 for success
+ **/
+char *_strchr(char *s, char c)
+{
+	int length;
+	for (length = 0; s[length] != '\0'; length++)
+	{
+		if (s[length] == c)
+		{
+			
+			return (s + length);
+		}
+	}
+return ('\0');
+
+}
