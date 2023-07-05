2585c0d2153e3ed2f99c4c3a3e17846a60ade408
Maggieboa <achymag2020@gmail.com>
2023-07-05 09:26:15 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ *_puts_recursion-starting point of our program to pring out a string of characters
+ *
+ *@s:placeholder variable for our characters
+ *
+ *
+ *Return: empty since its void
+ *
+ *
+ *
+ **/
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
