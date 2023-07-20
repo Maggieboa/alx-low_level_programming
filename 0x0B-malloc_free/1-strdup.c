a70f62f87813de399d4d490ec4ffe17cf64de9fa
Maggieboa <achymag2020@gmail.com>
2023-07-20 09:26:55 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include<stdlib.h>
+/**
+ **_strdup-function for printing a string
+ *@str: placeholder variable
+ *
+ *
+ *Return: duplicate for the pointer of our string or NUll if empty
+ **/
+char *_strdup(char *str)
+{
+unsigned int size;
+unsigned int length;
+char *duplicate;
+
+length = 0;
+if (str == 0)
+{
+	return (NULL);
+}
+
+for (size = 0; str[size]; size++)
+{
+	length++;	
+}
+duplicate = malloc(sizeof(char) * (length + 1));
+if(duplicate == 0)
+{
+	return (NULL);
+}
+for (size = 0; str[size]; size++)
+{
+	duplicate[size] = str[size];
+	duplicate[length] = '\0';
+}
+return (duplicate);
+}
