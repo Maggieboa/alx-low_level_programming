ed910e56896f416c405c111c2389687354d00557
Maggieboa <133254215+Maggieboa@users.noreply.github.com>
2023-08-03 12:35:05 +0300

    Delete 0x0B-malloc_free directory

--- 0x0B-malloc_free/main.h
+++
@@ -1,7 +0,0 @@
-#define MAIN_H__
-#ifdef MAIN_H_
-void simple_print_buffer(char *buffer, unsigned int size);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-#endif
