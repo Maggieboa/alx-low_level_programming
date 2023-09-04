6b37c185a21d35325767ec94a26492ceab5f069a
maggieboa <achymag2020@gmail.com>
2023-09-04 11:28:01 +0300

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,15 @@
 #include<stdlib.h>
 #include<stdio.h>
 /**
- *main-the main function of the program
- *argc:placeholder variable for calc. the number of functions;
- *argv:placeholder variable
- *Return:0 for success
- *
- **/
+*main-the main function of the program
+*argc:placeholder variable for calc. the number of functions
+*argv:placeholder variable
+*Return:0 for success
+**/
 int main(int __attribute__((unused)) argc, char *argv[])
 {
 	int i;
-
+	
 	i = 0;
 	printf("%s\n", argv[i]);
 	return (0);
