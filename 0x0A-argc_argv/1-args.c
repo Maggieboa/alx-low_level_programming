7f1ee405f013d22cc1cfd94c703bf97de451558a
Maggieux <achymag2020@gmail.com>
2023-09-06 17:45:59 +0300

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main-the main function
+*@argc:placeholder for number of arguments
+*@argv:placeholder for arguments in a functions
+*Return:0 for success
+*/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+
+if (argc != '\0')
+{
+	printf("%d\n", argc);
+}
+return (0);
+}
