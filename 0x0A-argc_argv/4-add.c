006b97a3cdf5015db6f723f5d0408e5f715a4878
Maggieboa <achymag2020@gmail.com>
2023-09-07 17:23:59 +0300

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+/**
+*main-main function of the program
+*@argc:placeholder variable for the number of arguments
+*@argv:placeholder varaible for the arguments
+*Return: 0 if success and 1 if not successful
+*/
+int main(int argc, char *argv[])
+{
+int sum, i, z;
+
+if (argc < 1)
+{
+	printf("0\n");
+}
+for (i = 1; i < argc; i++)
+{
+	for (z = 0; argv[i][z] != '\0'; z++)
+	{
+		if (!isdigit(argv[i][z]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		
+	}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+
+return (0);
+}
