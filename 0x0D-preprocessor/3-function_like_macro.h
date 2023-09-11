d580f248bb1ffbda09f162b2e253adecb5626a27
Maggieboa <achymag2020@gmail.com>
2023-09-11 20:02:11 +0300

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,7 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _FUNCTION_LIKE_MACRO_H_
+#define _FUNCTION_LIKE_MACRO_H_
+/*
+*ABS:convert an number into an absolute number
+*/
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 #endif
