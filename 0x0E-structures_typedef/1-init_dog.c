39df35c192fa46a32787f6e22d3b009396fde729
Maggieboa <achymag2020@gmail.com>
2023-09-12 20:09:17 +0300

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,14 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 /**
-*init_dog-main function
-*@d:placeholder variable
-*@name: placeholder variable name
-*@age:plcaholder variable age
-*@owner:placeholder variable owner
-*Return: void
-*/
+  *init_dog - initializes a variable of type struct dog.
+  *@d: pointer to structure.
+  *@name: first member.
+  *@age: second member.
+  *@owner: third member.
+  *
+  *Return: void.
+  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == 0)
