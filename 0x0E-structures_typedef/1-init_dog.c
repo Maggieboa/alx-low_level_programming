eaafc507f562383f59ecca5664290c1b6b16a41d
Maggieboa <achymag2020@gmail.com>
2023-09-12 20:05:41 +0300

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,14 +7,13 @@
 *@age:plcaholder variable age
 *@owner:placeholder variable owner
 *Return: void
-**/
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == 0)
-{
-	return;
-}
-d->name = name;
-d->age = age;
-d->owner = owner;
+	if (d == 0)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
