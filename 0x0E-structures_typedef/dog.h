62b691f54ddcac22e13ca8a6068b04c2b592ec81
Maggieboa <achymag2020@gmail.com>
2023-09-12 19:15:50 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H_
 #define DOG_H_
+/**
+  *struct dog - structure of the dog.
+  *@name: dog's name.
+  *@age: dog's age.
+  *@owner: dog's owner.
+  */
 struct dog
 {
 char *name;
 float age;
 char *owner;
+/* used for declaring variable name, age and owner */
 };
 #endif
