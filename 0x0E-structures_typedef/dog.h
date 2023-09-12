04efdfc37cd2d9b7541214618fc14de5b9406a9e
Maggieboa <achymag2020@gmail.com>
2023-09-12 20:43:33 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,11 @@
  *
  * Description: Defines the data about the dog its struct
  */
-typedef struct dog
+struct dog
 {
 char *name;
 float age;
 char *owner;
-/* used for declaring variable name, age and owner */
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
