a9cb31635dfeb792750c82f5967e9415c1f901af
Maggieboa <achymag2020@gmail.com>
2023-09-20 18:55:40 +0300

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*_puts_recursion-main function to print
+*@s:placeholder varaible
+*Return:return a string or -1 for ann error
+*
+*/
+void _puts_recursion(char *s)
+{
+int i;
+if (!*s)
+{
+	_putchar('E');
+	_putchar('R');
+	_putchar('R');
+	_putchar('O');
+	_putchar('R');
+	_putchar('\n');
+}
+i = 0;
+for (i = 0; s[i]; i++)
+{
+	_putchar(s[i]);
+}
+_putchar('\n');
+
+}
