b2b93b7b1417e5d6e960f744a85a7fdbaa058434
Maggieboa <133254215+Maggieboa@users.noreply.github.com>
2023-09-28 09:45:32 +0300

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/1-pi.h
+++
@@ -1,4 +0,0 @@
-#ifndef _MAIN_PI_H
-#define _MAIN_PI_H
-#define PI 3.14159265359
-#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-/*
-*ABS:convert an number into an absolute number
-*/
-#define ABS(x) (((x) < 0) ? (-x) : (x))
-#endif
