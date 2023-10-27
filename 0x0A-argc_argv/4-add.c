ce2d1494efc33699bcc870b3a7f536b5fb32bf67
Maggieboa <achymag2020@gmail.com>
2023-10-27 08:32:41 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<stdlib.h>
+/**
+ *main-function to calculate sum
+ *@argc:placeholder for # of args in cmd line
+ *@argv: cmd line args
+ *Return: 1 if error 0 if success
+ **/
+int main(int argc, char *argv[])
+{
+int i, sum = 0;
+char *str = "";
+
+for (i = 0; i < argc; i++)
+{
+	;
+}
+if (i <= 1)
+{
+	printf("0\n");
+}
+if (i > 1)
+{
+	for (i = 1; i < argc; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+printf("%d\n", sum);
+}
+return (0);
+}
