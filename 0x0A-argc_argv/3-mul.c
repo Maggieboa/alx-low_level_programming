df3353d5d16bb2ef368f6e02d80b21d73da46e66
Maggieboa <achymag2020@gmail.com>
2023-10-27 06:56:49 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-function to calculate multiplication of numbers
+ *@argc: placeholder for # ofargs on cmd
+ *@argv: placeholder to the args
+ *Return: 0 if successful,1 for error
+ **/
+int main(int argc, char* argv[])
+{
+int mul = 1, i;
+for (i = 0 ; i < argc; i++)
+{
+	;
+}
+	if (i >= 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	if (i < 2)
+	{
+		printf("Error\n");
+		return(1);
+	}
+return (0);
+}
