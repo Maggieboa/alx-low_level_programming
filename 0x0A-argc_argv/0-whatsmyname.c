63357ad735608c7e37b32a0df622b36ec62b4a13
Maggieboa <achymag2020@gmail.com>
2023-10-27 04:58:17 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main-main function
+ *@argc:counts the number of cmd  line arguments
+ *@argv: an array of arguments
+ *Return: 0 is successful
+ **/
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
