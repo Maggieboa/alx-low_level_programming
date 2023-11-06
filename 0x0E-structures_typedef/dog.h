3f7df64c74ffd6bea5722519e7c4e5d78a870ede
Maggieboa <achymag2020@gmail.com>
2023-11-06 07:55:01 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *struct dog-struct for defined data
+ *@name:placeholder for the name
+ *@age:placeholder for age
+ *@owner:placeholder for the owner
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
