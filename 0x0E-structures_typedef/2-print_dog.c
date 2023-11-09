cf9c1beeac07c06d86349469b1d318cb2873b3e4
Maggieboa <achymag2020@gmail.com>
2023-11-09 07:18:04 -0800

    2-print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *print_dog-function to print a series of string
+ *@d: struct
+ *Return: void
+ **/
+void print_dog(struct dog *d)
+{
+	char *name, *owner;
+	float age;
+	
+	name = (*d).name;
+	age = (*d).age;
+	owner = (*d).owner;
+	if (name == NULL)
+	{
+		printf("nil");
+	}
+	if (name)
+	{
+		printf("Name:%s \n", name);
+	}
+	if (age < 0)
+	{
+		printf("nil");
+	}
+	if (age >= 0)
+	{
+		printf("Age:%f \n", age);
+	}
+	if (owner == NULL)
+	{
+		printf("nil");
+	}
+	if (owner)
+	{
+		printf("Owner:%s\n", owner);
+	}
+}
