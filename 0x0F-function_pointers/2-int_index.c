fbda97d27cbacf0858e8347ab4d84b8c7fa62ad3
Maggieboa <achymag2020@gmail.com>
2024-03-03 16:16:04 +0300

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+*int_index-function tha prints the first index in an array
+*@array:placeholder for an array
+*@size:placeholder for the array size
+*@cmp:function to compare values
+*Return:1 if successfull -1 if there's an err
+**/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+	if (array != NULL || size > 0 || cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+	else
+	{
+		return (-1);
+	}
+	return (-1);
+}
