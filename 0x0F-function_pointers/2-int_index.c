8b048e1e82ba72def55184543b0d0a1231f58b6d
Maggieboa <achymag2020@gmail.com>
2024-03-04 18:59:23 +0300

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,7 +9,7 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-	if (array != NULL || size > 0 || cmp != NULL)
+	if (array != NULL || cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
