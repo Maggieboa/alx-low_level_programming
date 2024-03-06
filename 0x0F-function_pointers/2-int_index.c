dd84b24e4031bdf916a8db4a31aaeb1731350d0f
Maggieboa <achymag2020@gmail.com>
2024-03-06 17:41:57 +0300

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,19 +9,22 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-	if (array != NULL || cmp != NULL)
+	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		if (array != NULL || cmp != NULL)
 		{
-			if (cmp(array[i]) != 0)
+			for (i = 0; i < size; i++)
 			{
-				return (i);
+				if (cmp(array[i]) != 0)
+				{
+					return (i);
+				}
 			}
 		}
-	}
-	else
-	{
-		return (-1);
+		else
+		{
+			return (-1);
+		}
 	}
 	return (-1);
 }
